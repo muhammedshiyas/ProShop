93624ed4c9883e9434e51fa0f31e000b216d3c8d
shiyas <shiyastroxic@gmail.com>
2022-09-05 01:18:04 +0530

    test commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container } from 'react-bootstrap'
+import {  Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+import CartScreen from './screens/CartScreen'
+import LoginScreen from './screens/LoginScreen'
+import RegisterScreen from './screens/RegisterScreen'
+import ProfileScreen from './screens/ProfileScreen'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    
+      <Header />
+      <main className='py-3'>
+        <Container>
+        <Routes>
+          <Route exact path='/' element= {<HomeScreen />} />
+          <Route path='/login' element= {<LoginScreen />} />
+          <Route path='/register' element= {<RegisterScreen />} />
+          <Route path='/profile' element= {<ProfileScreen />} />
+          <Route path='/product/:id' element={<ProductScreen/>} />
+          <Route path='/cart/:id' element={<CartScreen/>} />
+          <Route path='/cart' element={<CartScreen/>} />
+          </Routes>
+        </Container>
+
+      </main>
+      <Footer />
+      </>
+   
   );
 }
 
